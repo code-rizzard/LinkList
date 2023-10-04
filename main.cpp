63da3a305e5adb70f3ebc2a6d234688e76b11746
Richard <richard232manansala@gmail.com>
2023-10-04 21:13:01 +0800

    Added insertAt func

--- main.cpp
+++ main.cpp
@@ -2,12 +2,71 @@
 
 using namespace std;
 
+
+
 class Node
 {
 public:
     Node *next;
     int data;
 };
+
+
+void insertLast(int data, Node *&head, Node *&last);
+
+void insertAt(int data, int position, Node *&head, Node *&last)
+{
+    if(position < 0) {
+        cout << "Negative position is invalid." << endl;
+        return;
+    }
+    if (head == nullptr)
+    {
+        head = new Node();
+        head->data = data;
+        head->next = nullptr;
+        last = head;
+        return;
+    }
+    int currentPos = 0;
+
+
+    if(position == 0) {
+        Node *temp = new Node();
+        temp->data = data;
+        temp->next = head;
+        head = temp;
+        return;
+    }
+
+    Node *node = head;
+    Node *prevNode = nullptr;
+    do
+    {
+        if (currentPos == position)
+        {
+            Node *temp = new Node();
+            temp->data = data;
+            prevNode->next = temp;
+            temp->next=node;
+            return;
+        }
+        prevNode = node;
+        node = node->next;
+        if(node != nullptr) {
+            currentPos++;
+
+        }
+    } while (node != nullptr);
+    if((position - currentPos) > 1) {
+    cout << "Position is far too large.";
+    } else {
+        cout << "POs" << currentPos << endl;
+        insertLast(data, head, last);
+    }
+
+
+}
 
 void insertLast(int data, Node *&head, Node *&last)
 {
@@ -80,7 +139,8 @@ void removeLast(Node *&head, Node *&last)
 void showItems(Node *head)
 {
     Node *temp = head;
-    cout << "Linked List: ";
+
+    cout << endl<< "Linked List: ";
     if (head == nullptr)
     {
         cout << "empty";
@@ -100,26 +160,33 @@ int main()
     Node *head = nullptr;
     Node *last = nullptr;
 
+
     cout << "Linked List is empty." << endl;
     bool loop = true;
     do
     {
         int action;
-        cout << "1. Insert \n2. Delete \n3. Show All \n0. Exit \n";
+        cout << "\n1. Insert \n2. Delete \n0. Exit \n\nInput: ";
         cin >> action;
         switch (action)
         {
         case 1:
             int data;
-            cout << "Enteasdfr data: ";
+            cout << "Enter data: ";
             cin >> data;
-            insertLast(data, head, last);
+            int pos;
+            if(head != nullptr) {
+                showItems(head);
+                cout << "Enter position (0-index): ";
+                cin >> pos;
+                insertAt(data, pos,head, last);
+
+            } else {
+                insertLast(data,head,last);
+            }
             showItems(head);
             break;
         case 2:
-            break;
-        case 3:
-            showItems(head);
             break;
         case 0:
             loop = false;
